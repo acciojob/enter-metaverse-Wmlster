dc95e2a885558425ac5fc586286d8aba46a31e8f
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-23 15:48:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let para=document.getElementById("status");
 
 
-enterBtn.addEventListener("click",function(){
+document.getElementById("enterBtn").addEventListener("click",function(){
 
 	let heading=document.createElement("h1");
 
