5b738e4f0314c46f6081c6bdc5e5b861c2f3980c
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-23 16:25:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let para=document.getElementById('status');
+let para=document.getElementById('status'); 
   enterBtn.addEventListener("click",function(){
 	  let heading=document.createElement("h1");
 	  heading.innerText="Entered Metaverse";
