2a1307a78b019e07a59418106135942780c277fd
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-23 16:21:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let para=document.getElementById("status");
-
-
-enterBtn.addEventListener("click",function(){
-
-	let heading=document.createElement("h1");
-
-	heading.innerText="Entered Metaverse";
-
-	para.innerText="";
-	para.appendChild(heading);
-	
-})
+let para=document.getElementById('status');
+  enterBtn.addEventListener("click",function(){
+	  let heading=document.createElement("h1");
+	  heading.innerText="Entered Metaverse";
+	  para.innerText="";
+	  para.appendChild(heading);
+  })
